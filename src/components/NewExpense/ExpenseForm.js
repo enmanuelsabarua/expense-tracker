cd3f1c5ce7cab2f70dd1dce9f2498799d0dffbc2
Abarua <enmanuel.sanchez.a@gmail.com>
2022-06-12 15:58:01 -0400

    Add new item form

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = props => {
+    const [userInput, setUserInput] = useState({
+        title: '',
+        amount: '',
+        date: ''
+    });
+
+    const titleChangeHandler = e => {
+        setUserInput(prevState => {
+            return {
+                ...prevState,
+                title: e.target.value
+            }
+        });
+    }
+
+    const amountChangeHandler = e => {
+        setUserInput(prevState => {
+            return {
+                ...prevState,
+                amount: e.target.value
+            }
+        });
+    }
+
+    const dateChangeHandler = e => {
+        setUserInput(prevState => {
+            return {
+                ...prevState,
+                date: new Date(e.target.value)
+            }
+        });
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        props.onSaveExpenseData(userInput);
+
+        setUserInput({
+            title: '',
+            amount: '',
+            date: ''
+        });
+    }
+    
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input type="text" value={userInput.title} onChange={titleChangeHandler}/>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01" value={userInput.amount} onChange={amountChangeHandler}/>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input type="date" min="2019-01-01" max="2023-12-31" value={userInput.date} onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    );
+}
+
+export default ExpenseForm;
